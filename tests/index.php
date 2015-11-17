f57b4833be6398223bfd5fb78b17ba11e91f25fe
Alewex <alex.eepsilon@gmail.com>
2015-11-16 20:31:31 -0600

    Update index.php

--- tests/index.php
+++ tests/index.php
@@ -5,6 +5,16 @@ require '../vendor/autoload.php';
 
 $halo = new H5Api\HaloAPI('b6fbbdbe541444c5824e3cb1e74f313b');
 
-$nadedProfile = $halo->profile('naded');
+$naded = $halo->player('naded');
+$goofy = $halo->player('g00fy');
 
-var_dump($nadedProfile->getEmblem());
+$nadedEmblem = $naded->profile->getEmblem(190);
+$goofyEmblem = $goofy->profile->getEmblem(190);
+
+$nadedMatches = $naded->matches->getMatches();
+$goofyMatches = $goofy->matches->getMatches();
+
+echo <<<EOF
+	<img src="{$nadedEmblem}" alt="{$naded->gamertag}"></img>
+	<img src="{$goofyEmblem}" alt="{$goofy->gamertag}"></img>
+EOF;
