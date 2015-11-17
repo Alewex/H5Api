350a6e58b98cfbef6476002e31171684f2c5c5c6
Alewex <alex.eepsilon@gmail.com>
2015-11-16 20:32:14 -0600

    Update Request class

--- src/H5Api/API/Request.php
+++ src/H5Api/API/Request.php
@@ -4,8 +4,22 @@ namespace H5Api\API;
 
 class Request {
 
+	public $response, $responseInfo;
+
 	public function __construct($url, $key) {
-		return $key;
+		// Init cURL
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, ["Ocp-Apim-Subscription-Key: {$key}"]);
+
+		// Execute cURL
+		$this->response = curl_exec($curl);
+		$this->responseInfo = curl_getinfo($curl);
+		curl_close($curl);
+
+		return $this;
 	}
 
 }
